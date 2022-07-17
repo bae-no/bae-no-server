ba2bc9834ab834fc0fc588fe2fce3927e5027aa0
Jake Son <sonjeabin@gmail.com>
2022-07-17 18:14:09 +0900

    🩹 응답 id 타입 수정

--- apps/app/src/module/sample/adapter/in/gql/response/SampleResponse.ts
+++ apps/app/src/module/sample/adapter/in/gql/response/SampleResponse.ts
@@ -1,10 +1,10 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 import { Sample } from '../../../../domain/Sample';
 
 @ObjectType()
 export class SampleResponse {
-  @Field()
+  @Field(() => ID)
   id: string;
 
   @Field()

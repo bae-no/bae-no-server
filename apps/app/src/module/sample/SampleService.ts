128b66b70f73b7256d87d17918e74c580a2329ce
Jake Son <sonjeabin@gmail.com>
2022-07-17 10:04:00 +0900

    ✨ 샘플 prisma 코드 추가

--- apps/app/src/module/sample/SampleService.ts
+++ apps/app/src/module/sample/SampleService.ts
@@ -1,4 +1,14 @@
+import { PrismaService } from '@app/prisma/PrismaService';
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
-export class SampleService {}
+export class SampleService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+}

18fee1ec178bd63aa237cb1f555ed9d5937f75c0
Jake Son <sonjeabin@gmail.com>
2022-07-23 14:10:39 +0900

    ✨ 샘플 레포지토리 task either 적용

--- apps/app/src/module/sample/adapter/out/persistence/SampleQueryRepositoryAdapter.ts
+++ apps/app/src/module/sample/adapter/out/persistence/SampleQueryRepositoryAdapter.ts
@@ -1,6 +1,12 @@
+import { DBError, tryCatchDB } from '@app/domain/error/DBError';
 import { PrismaService } from '@app/prisma/PrismaService';
 import { Injectable } from '@nestjs/common';
 import { Sample as OrmSample } from '@prisma/client';
+import { pipe } from 'fp-ts/function';
+import { Option } from 'fp-ts/Option';
+import * as O from 'fp-ts/Option';
+import * as TE from 'fp-ts/TaskEither';
+import { TaskEither } from 'fp-ts/TaskEither';
 
 import { SampleQueryRepositoryPort } from '../../../application/port/out/SampleQueryRepositoryPort';
 import { Sample } from '../../../domain/Sample';
@@ -11,17 +17,17 @@ export class SampleQueryRepositoryAdapter extends SampleQueryRepositoryPort {
     super();
   }
 
-  override async findById(id: string): Promise<Sample | null> {
-    return this.prisma.sample
-      .findFirst({ where: { id } })
-      .then(this.toDomainSample);
+  override findById(id: string): TaskEither<DBError, Option<Sample>> {
+    return pipe(
+      tryCatchDB(() => this.prisma.sample.findFirst({ where: { id } })),
+      TE.map(this.toDomainSample),
+    );
   }
 
-  private toDomainSample(ormEntity: OrmSample | null): Sample | null {
-    if (!ormEntity) {
-      return null;
-    }
-
-    return Object.assign(Sample.empty(), ormEntity);
+  private toDomainSample(ormEntity: OrmSample | null): Option<Sample> {
+    return pipe(
+      O.fromNullable(ormEntity),
+      O.map((v) => Object.assign(Sample.empty(), v)),
+    );
   }
 }

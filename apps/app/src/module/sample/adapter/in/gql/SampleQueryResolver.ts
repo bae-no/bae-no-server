1b8d41bad14ab4e9b31d4795922feaf995c590a2
Jake Son <sonjeabin@gmail.com>
2022-07-23 14:11:01 +0900

    ✨ 샘플 리졸버 task either 적용

--- apps/app/src/module/sample/adapter/in/gql/SampleQueryResolver.ts
+++ apps/app/src/module/sample/adapter/in/gql/SampleQueryResolver.ts
@@ -1,4 +1,6 @@
 import { Args, ID, Query, Resolver } from '@nestjs/graphql';
+import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
 
 import { SampleQueryUseCase } from '../../../application/port/in/SampleQueryUseCase';
 import { SampleResponse } from './response/SampleResponse';
@@ -11,6 +13,12 @@ export class SampleQueryResolver {
   findById(
     @Args({ name: 'id', type: () => ID }) id: string,
   ): Promise<SampleResponse> {
-    return this.sampleQueryUseCase.findById(id).then(SampleResponse.of);
+    return pipe(
+      this.sampleQueryUseCase.findById(id),
+      TE.map(SampleResponse.of),
+      TE.getOrElse((err) => {
+        throw err;
+      }),
+    )();
   }
 }

04fbcb14ae82bdd91c01f420331d892e4f485179
Yohan <cd80@kakao.com>
2022-08-06 09:19:13 +0900

    📦 Config 라이브러리 추가

--- apps/app/src/AppModule.ts
+++ apps/app/src/AppModule.ts
@@ -4,6 +4,7 @@ import { PrismaModule } from '@app/prisma/PrismaModule';
 import { PubSubModule } from '@app/pub-sub/PubSubModule';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
@@ -23,6 +24,9 @@ import { UserModule } from './module/user/UserModule';
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       introspection: true,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
     PrismaModule,
     PubSubModule,

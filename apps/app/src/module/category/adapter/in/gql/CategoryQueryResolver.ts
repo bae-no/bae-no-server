86c52eb7f517f54fc08ba831a44cb6151347812f
Jake Son <sonjeabin@gmail.com>
2022-08-07 18:22:08 +0900

    🩹 public api 설정

--- apps/app/src/module/category/adapter/in/gql/CategoryQueryResolver.ts
+++ apps/app/src/module/category/adapter/in/gql/CategoryQueryResolver.ts
@@ -1,9 +1,13 @@
+import './registerEnum';
+
 import { Query, Resolver } from '@nestjs/graphql';
 
+import { Public } from '../../../../user/adapter/in/gql/auth/Public';
 import { CategoryResponse } from './response/CategoryResponse';
 
 @Resolver()
 export class CategoryQueryResolver {
+  @Public()
   @Query(() => CategoryResponse, { description: '카테고리 목록' })
   categories(): CategoryResponse {
     return CategoryResponse.VALUES;

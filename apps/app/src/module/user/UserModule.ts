e722919e32b71e2d2296a1513e32f844f4c4056e
Jake Son <sonjeabin@gmail.com>
2022-08-07 12:07:04 +0900

    ✨ 사용자 query repository 구현

--- apps/app/src/module/user/UserModule.ts
+++ apps/app/src/module/user/UserModule.ts
@@ -4,8 +4,10 @@ import { Module } from '@nestjs/common';
 import { UserMutationResolver } from './adapter/in/gql/UserMutationResolver';
 import { AuthProvider } from './adapter/out/auth/AuthProvider';
 import { KakaoAuthStrategy } from './adapter/out/auth/strategy/KakaoStrategy';
+import { UserQueryRepositoryAdapter } from './adapter/out/persistence/UserQueryRepositoryAdapter';
 import { UserCommandUseCase } from './application/port/in/UserCommandUseCase';
 import { AuthProviderPort } from './application/port/out/AuthProviderPort';
+import { UserQueryRepositoryPort } from './application/port/out/UserQueryRepositoryPort';
 import { UserCommandService } from './application/service/UserCommandService';
 
 @Module({
@@ -18,6 +20,10 @@ import { UserCommandService } from './application/service/UserCommandService';
     {
       provide: AuthProviderPort,
       useClass: AuthProvider,
+    },
+    {
+      provide: UserQueryRepositoryPort,
+      useClass: UserQueryRepositoryAdapter,
     },
     KakaoAuthStrategy,
   ],
---
+++ apps/app/src/module/user/adapter/out/persistence/UserQueryRepositoryAdapter.ts
@@ -0,0 +1,40 @@
+import { DBError, tryCatchDB } from '@app/domain/error/DBError';
+import { TE, O } from '@app/domain/fp-ts';
+import { PrismaService } from '@app/prisma/PrismaService';
+import { Injectable } from '@nestjs/common';
+import { pipe } from 'fp-ts/function';
+import { Option } from 'fp-ts/Option';
+import { TaskEither } from 'fp-ts/TaskEither';
+
+import { UserQueryRepositoryPort } from '../../../application/port/out/UserQueryRepositoryPort';
+import { User } from '../../../domain/User';
+import { Auth } from '../../../domain/vo/Auth';
+import { UserOrmMapper } from './UserOrmMapper';
+
+@Injectable()
+export class UserQueryRepositoryAdapter extends UserQueryRepositoryPort {
+  constructor(private readonly prisma: PrismaService) {
+    super();
+  }
+
+  findByAuth(auth: Auth): TaskEither<DBError, Option<User>> {
+    return pipe(
+      tryCatchDB(() =>
+        this.prisma.user.findFirst({
+          where: {
+            auth: {
+              socialId: auth.socialId,
+              type: auth.type,
+            },
+          },
+        }),
+      ),
+      TE.map((ormUser) =>
+        pipe(
+          O.fromNullable(ormUser),
+          O.map((user) => UserOrmMapper.toDomain(user)),
+        ),
+      ),
+    );
+  }
+}

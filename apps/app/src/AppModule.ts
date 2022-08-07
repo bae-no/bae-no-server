aec6ce1b73c136bd11c736a779665e531c99018c
Jake Son <sonjeabin@gmail.com>
2022-08-07 18:21:20 +0900

    ✨ 인가 설정

--- apps/app/src/AppModule.ts
+++ apps/app/src/AppModule.ts
@@ -5,11 +5,13 @@ import { PubSubModule } from '@app/pub-sub/PubSubModule';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 import { CategoryModule } from './module/category/CategoryModule';
 import { SampleModule } from './module/sample/SampleModule';
+import { GqlAuthGuard } from './module/user/adapter/in/gql/auth/GqlAuthGuard';
 import { UserModule } from './module/user/UserModule';
 
 @Module({
@@ -33,6 +35,12 @@ import { UserModule } from './module/user/UserModule';
     CategoryModule,
     SampleModule,
     UserModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: GqlAuthGuard,
+    },
   ],
 })
 export class AppModule {}
---
+++ apps/app/src/module/user/adapter/in/gql/auth/GqlAuthGuard.ts
@@ -0,0 +1,32 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { AuthGuard } from '@nestjs/passport';
+
+import { IS_PUBLIC_KEY } from './Public';
+
+@Injectable()
+export class GqlAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+
+    return ctx.getContext().req;
+  }
+
+  override canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+}

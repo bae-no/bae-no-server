c13fa934c490f00b7c7866d11df56602d219908f
Jake Son <sonjeabin@gmail.com>
2022-08-07 20:48:44 +0900

    ✨ 전화 인증 클래스 구현

---
+++ apps/app/src/module/user/domain/PhoneVerification.ts
@@ -0,0 +1,24 @@
+import { IllegalStateException } from '@app/domain/exception/IllegalStateException';
+import { isAfter } from 'date-fns';
+
+export class PhoneVerification {
+  constructor(readonly code: string, readonly expiredAt: Date) {}
+
+  verify(code: string, now = new Date()) {
+    if (this.code !== code) {
+      throw new IllegalStateException(
+        `코드가 일치하지 않습니다: expected=${this.code}, actual=${code}`,
+      );
+    }
+
+    if (this.isExpired(now)) {
+      throw new IllegalStateException(
+        `코드가 만료되었습니다: expiredAt=${this.expiredAt}`,
+      );
+    }
+  }
+
+  private isExpired(now: Date): boolean {
+    return isAfter(now, this.expiredAt);
+  }
+}
---
+++ apps/app/test/user/unit/PhoneVerification.spec.ts
@@ -0,0 +1,46 @@
+import { IllegalStateException } from '@app/domain/exception/IllegalStateException';
+import { addSeconds } from 'date-fns';
+
+import { PhoneVerification } from '../../../src/module/user/domain/PhoneVerification';
+
+describe('PhoneVerification', () => {
+  describe('verify', () => {
+    it('주어진 code 와 일치하지 않으면 에러가 발생한다', () => {
+      // given
+      const verification = new PhoneVerification('1234', new Date());
+
+      // when
+      const verify = () => verification.verify('5678');
+
+      // then
+      expect(verify).toThrow(IllegalStateException);
+    });
+
+    it('만료되었다면 code 와 일치해도 만료된 경우 에러가 발생한다', () => {
+      // given
+      const now = new Date();
+      const verification = new PhoneVerification('1234', now);
+
+      // when
+      const verify = () => verification.verify('1234', addSeconds(now, 1));
+
+      // then
+      expect(verify).toThrow(IllegalStateException);
+    });
+
+    it('주어진 code 와 일치하고 만료되지 않으면 성공한다', () => {
+      // given
+      const verification = new PhoneVerification(
+        '1234',
+        new Date('2022-03-03 12:00:00'),
+      );
+
+      // when
+      const verify = () =>
+        verification.verify('1234', new Date('2022-03-03 11:59:59'));
+
+      // then
+      expect(verify).not.toThrow();
+    });
+  });
+});

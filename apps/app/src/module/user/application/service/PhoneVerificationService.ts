37d774ea60cd620e450944d26217b6654cdb47d5
Jake Son <sonjeabin@gmail.com>
2022-08-13 10:20:27 +0900

    ✨ 인증번호 검증 서비스 구현

--- apps/app/src/module/user/application/service/PhoneVerificationService.ts
+++ apps/app/src/module/user/application/service/PhoneVerificationService.ts
@@ -3,18 +3,26 @@ import { NotificationError } from '@app/domain/error/NotificationError';
 import { SmsPort } from '@app/domain/notification/SmsPort';
 import { TE } from '@app/external/fp-ts';
 import { Injectable } from '@nestjs/common';
-import { pipe } from 'fp-ts/function';
+import { constVoid, pipe } from 'fp-ts/function';
 import { TaskEither } from 'fp-ts/TaskEither';
 
 import { PhoneVerification } from '../../domain/PhoneVerification';
-import { PhoneVerificationUseCase } from '../port/in/PhoneVerificationUseCase';
+import {
+  PhoneVerificationUseCase,
+  VerifyPhoneVerificationCodeError,
+} from '../port/in/PhoneVerificationUseCase';
 import { SendPhoneVerificationCodeCommand } from '../port/in/SendPhoneVerificationCodeCommand';
+import { VerifyPhoneVerificationCodeCommand } from '../port/in/VerifyPhoneVerificationCodeCommand';
 import { PhoneVerificationRepositoryPort } from '../port/out/PhoneVerificationRepositoryPort';
+import { UserQueryRepositoryPort } from '../port/out/UserQueryRepositoryPort';
+import { UserRepositoryPort } from '../port/out/UserRepositoryPort';
 
 @Injectable()
 export class PhoneVerificationService extends PhoneVerificationUseCase {
   constructor(
     private readonly phoneVerificationRepositoryPort: PhoneVerificationRepositoryPort,
+    private readonly userQueryRepositoryPort: UserQueryRepositoryPort,
+    private readonly userRepositoryPort: UserRepositoryPort,
     private readonly smsPort: SmsPort,
   ) {
     super();
@@ -31,6 +39,24 @@ export class PhoneVerificationService extends PhoneVerificationUseCase {
       TE.chainW((verification) =>
         this.smsPort.send(verification.phoneNumber, verification.code),
       ),
+    );
+  }
+
+  override verify(
+    command: VerifyPhoneVerificationCodeCommand,
+  ): TaskEither<VerifyPhoneVerificationCodeError, void> {
+    return pipe(
+      TE.Do,
+      TE.apS(
+        'verification',
+        this.phoneVerificationRepositoryPort.findLatest(command.userId),
+      ),
+      TE.apS('user', this.userQueryRepositoryPort.findById(command.userId)),
+      TE.chainEitherKW(({ verification, user }) =>
+        user.updateByPhoneVerification(verification, command.code),
+      ),
+      TE.chain((user) => this.userRepositoryPort.save(user)),
+      TE.map(constVoid),
     );
   }
 }

41e013b15a62893c5e6c4f9efb9e7975dd03f45e
Jake Son <sonjeabin@gmail.com>
2022-09-03 20:51:32 +0900

    ⬆️ got 버전 업데이트

--- libs/http-client/src/HttpClientModule.ts
+++ libs/http-client/src/HttpClientModule.ts
@@ -3,8 +3,17 @@ import { Module } from '@nestjs/common';
 
 import { HttpClientService } from './HttpClientService';
 
+const dynamicImport = async (packageName: string) =>
+  new Function(`return import('${packageName}')`)();
+
 @Module({
-  providers: [{ provide: HttpClientPort, useClass: HttpClientService }],
+  providers: [
+    {
+      provide: HttpClientPort,
+      useFactory: async () =>
+        new HttpClientService((await dynamicImport('got')).default),
+    },
+  ],
   exports: [HttpClientPort],
 })
 export class HttpClientModule {}

76feaffec235ac9a14a72e61530125316f5581d6
yohan <cd80@kakao.com>
2022-10-09 16:39:25 +0900

    ✅ 채팅방 참여 서비스 테스트 코드 추가
    Co-authored-by: Jake Son <sonjeabin@gmail.com>

--- apps/app/test/share-deal/unit/ShareDealCommandService.spec.ts
+++ apps/app/test/share-deal/unit/ShareDealCommandService.spec.ts
@@ -1,20 +1,28 @@
 import { right } from 'fp-ts/TaskEither';
 import { mock, mockReset } from 'jest-mock-extended';
 
+import { JoinShareDealCommand } from '../../../src/module/share-deal/application/port/in/dto/JoinShareDealCommand';
 import { OpenShareDealCommand } from '../../../src/module/share-deal/application/port/in/dto/OpenShareDealCommand';
+import { NotJoinableShareDealException } from '../../../src/module/share-deal/application/port/in/exception/NotJoinableShareDealException';
+import { ShareDealQueryRepositoryPort } from '../../../src/module/share-deal/application/port/out/ShareDealQueryRepositoryPort';
 import { ShareDealRepositoryPort } from '../../../src/module/share-deal/application/port/out/ShareDealRepositoryPort';
 import { ShareDealCommandService } from '../../../src/module/share-deal/application/service/ShareDealCommandService';
 import { FoodCategory } from '../../../src/module/share-deal/domain/vo/FoodCategory';
-import { assertResolvesRight } from '../../fixture';
+import { ShareDealStatus } from '../../../src/module/share-deal/domain/vo/ShareDealStatus';
+import { assertResolvesLeft, assertResolvesRight } from '../../fixture';
+import { ShareDealFactory } from '../../fixture/ShareDealFactory';
 
 describe('ShareDealCommandService', () => {
   const shareDealRepositoryPort = mock<ShareDealRepositoryPort>();
+  const shareDealQueryRepositoryPort = mock<ShareDealQueryRepositoryPort>();
   const shareDealCommandService = new ShareDealCommandService(
     shareDealRepositoryPort,
+    shareDealQueryRepositoryPort,
   );
 
   beforeEach(() => {
     mockReset(shareDealRepositoryPort);
+    mockReset(shareDealQueryRepositoryPort);
   });
 
   describe('open', () => {
@@ -41,6 +49,45 @@ describe('ShareDealCommandService', () => {
 
       // then
       await assertResolvesRight(result);
+    });
+  });
+
+  describe('join', () => {
+    it('채팅 참여가 불가능한 공유딜에 참여 요청할 경우 NotOpenShareDealException이 반환된다.', async () => {
+      // given
+      const command = new JoinShareDealCommand('userId', 'shareDealId');
+      const shareDeal = ShareDealFactory.create({
+        status: ShareDealStatus.START,
+      });
+
+      shareDealQueryRepositoryPort.findById.mockReturnValue(right(shareDeal));
+
+      // when
+      const result = shareDealCommandService.join(command);
+
+      // then
+      await assertResolvesLeft(result, (value) => {
+        expect(value).toBeInstanceOf(NotJoinableShareDealException);
+      });
+    });
+
+    it('공유딜 채팅방 참여를 요청한다.', async () => {
+      // given
+      const command = new JoinShareDealCommand('userId', 'shareDealId');
+      const shareDeal = ShareDealFactory.create({
+        status: ShareDealStatus.OPEN,
+      });
+
+      shareDealQueryRepositoryPort.findById.mockReturnValue(right(shareDeal));
+      shareDealRepositoryPort.save.mockReturnValue(right(shareDeal));
+
+      // when
+      const result = shareDealCommandService.join(command);
+
+      // then
+      await assertResolvesRight(result, () => {
+        expect(shareDeal.participantInfo.hasId(command.userId)).toBe(true);
+      });
     });
   });
 });

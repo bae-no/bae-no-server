6e35919a14a7b589ef450781426debf86017451c
yohan <cd80@kakao.com>
2022-10-09 11:42:17 +0900

    ✅ Query, Form 데이터 요청 테스트 추가
    Co-authored-by: Jake Son <sonjeabin@gmail.com>

--- libs/http-client/test/unit/HttpClientService.spec.ts
+++ libs/http-client/test/unit/HttpClientService.spec.ts
@@ -46,4 +46,52 @@ describe('HttpClientService', () => {
     // then
     await assertResolvesLeft(result);
   }, 10000);
+
+  it('Query 요청에 대해 응답한다.', async () => {
+    // given
+    const url = 'http://localhost:8080/param';
+    const query = { foo: 'bar' };
+
+    // when
+    const result = httpClientService.get(url, { query });
+
+    // then
+    await assertResolvesRight(result, (response) => {
+      expect(response.body).toMatchInlineSnapshot(
+        `"{"query":{"foo":"bar"},"body":{}}"`,
+      );
+    });
+  });
+
+  it('Form 요청에 대해 응답한다.', async () => {
+    // given
+    const url = 'http://localhost:8080/param';
+    const form = { foo: 'bar' };
+
+    // when
+    const result = httpClientService.post(url, { form });
+
+    // then
+    await assertResolvesRight(result, (response) => {
+      expect(response.body).toMatchInlineSnapshot(
+        `"{"query":{},"body":{"foo":"bar"}}"`,
+      );
+    });
+  });
+
+  it('Body 요청에 대해 응답한다.', async () => {
+    // given
+    const url = 'http://localhost:8080/param';
+    const body = { foo: 'bar' };
+
+    // when
+    const result = httpClientService.post(url, { body });
+
+    // then
+    await assertResolvesRight(result, (response) => {
+      expect(response.body).toMatchInlineSnapshot(
+        `"{"query":{},"body":{"foo":"bar"}}"`,
+      );
+    });
+  });
 });

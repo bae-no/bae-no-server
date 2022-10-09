f5e7d723fb440f3b1f551fb09bd11faa657b101c
yohan <cd80@kakao.com>
2022-10-09 16:22:17 +0900

    ✨ 채팅방 참여 서비스 구현
    Co-authored-by: Jake Son <sonjeabin@gmail.com>

--- apps/app/src/module/share-deal/application/port/in/ShareDealCommandUseCase.ts
+++ apps/app/src/module/share-deal/application/port/in/ShareDealCommandUseCase.ts
@@ -1,8 +1,18 @@
 import { DBError } from '@app/domain/error/DBError';
+import { NotFoundException } from '@app/domain/exception/NotFoundException';
 import { TaskEither } from 'fp-ts/TaskEither';
 
+import { JoinChatCommand } from './dto/JoinChatCommand';
 import { OpenShareDealCommand } from './dto/OpenShareDealCommand';
+import { NotOpenShareDealException } from './exception/NotOpenShareDealException';
+
+export type JoinChatError =
+  | DBError
+  | NotFoundException
+  | NotOpenShareDealException;
 
 export abstract class ShareDealCommandUseCase {
   abstract open(command: OpenShareDealCommand): TaskEither<DBError, void>;
+
+  abstract join(command: JoinChatCommand): TaskEither<JoinChatError, void>;
 }
--- apps/app/src/module/share-deal/application/service/ShareDealCommandService.ts
+++ apps/app/src/module/share-deal/application/service/ShareDealCommandService.ts
@@ -4,14 +4,21 @@ import { Injectable } from '@nestjs/common';
 import { constVoid, pipe } from 'fp-ts/function';
 import { TaskEither } from 'fp-ts/TaskEither';
 
+import { JoinChatCommand } from '../port/in/dto/JoinChatCommand';
 import { OpenShareDealCommand } from '../port/in/dto/OpenShareDealCommand';
-import { ShareDealCommandUseCase } from '../port/in/ShareDealCommandUseCase';
+import { NotOpenShareDealException } from '../port/in/exception/NotOpenShareDealException';
+import {
+  JoinChatError,
+  ShareDealCommandUseCase,
+} from '../port/in/ShareDealCommandUseCase';
+import { ShareDealQueryRepositoryPort } from '../port/out/ShareDealQueryRepositoryPort';
 import { ShareDealRepositoryPort } from '../port/out/ShareDealRepositoryPort';
 
 @Injectable()
 export class ShareDealCommandService extends ShareDealCommandUseCase {
   constructor(
     private readonly shareDealRepositoryPort: ShareDealRepositoryPort,
+    private readonly shareDealQueryRepositoryPort: ShareDealQueryRepositoryPort,
   ) {
     super();
   }
@@ -19,6 +26,18 @@ export class ShareDealCommandService extends ShareDealCommandUseCase {
   override open(command: OpenShareDealCommand): TaskEither<DBError, void> {
     return pipe(
       this.shareDealRepositoryPort.save(command.toDomain()),
+      TE.map(constVoid),
+    );
+  }
+
+  override join(command: JoinChatCommand): TaskEither<JoinChatError, void> {
+    return pipe(
+      this.shareDealQueryRepositoryPort.findById(command.shareDealId),
+      TE.filterOrElseW(
+        (deal) => deal.isJoinable,
+        () => new NotOpenShareDealException('입장 가능한 공유딜이 아닙니다.'),
+      ),
+      TE.chain((deal) => this.shareDealRepositoryPort.save(deal)),
       TE.map(constVoid),
     );
   }

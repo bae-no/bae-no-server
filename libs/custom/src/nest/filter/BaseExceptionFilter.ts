068841c11eefc35c9594c0136bc323fb48ec3587
yohan <cd80@kakao.com>
2022-10-09 17:40:29 +0900

    🩹 전역 필터링 에러 핸들러 개선
    Co-authored-by: Jake Son <sonjeabin@gmail.com>

--- libs/custom/src/nest/filter/BaseExceptionFilter.ts
+++ libs/custom/src/nest/filter/BaseExceptionFilter.ts
@@ -5,6 +5,8 @@ import { ApolloError } from 'apollo-server-core';
 @Catch(BaseException)
 export class BaseExceptionFilter implements ExceptionFilter<BaseException> {
   catch(exception: BaseException): any {
-    throw new ApolloError(exception.message, exception.code);
+    const error = new ApolloError(exception.message, exception.code);
+    error.stack = exception.stack;
+    throw error;
   }
 }

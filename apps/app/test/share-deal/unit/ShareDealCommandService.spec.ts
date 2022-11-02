1c700b65ca807c4e20a83e2e3b252d3bd81aebd8
Yohan <cd80@kakao.com>
2022-11-02 21:27:18 +0900

    ✅ 공유딜 시작 서비스 레이어 테스트 코드 추가
    Co-authored-by: Jake Son <sonjeabin@gmail.com>

--- apps/app/test/share-deal/unit/ShareDealCommandService.spec.ts
+++ apps/app/test/share-deal/unit/ShareDealCommandService.spec.ts
@@ -1,13 +1,18 @@
+import { IllegalStateException } from '@app/domain/exception/IllegalStateException';
 import { right } from 'fp-ts/TaskEither';
 import { mock, mockReset } from 'jest-mock-extended';
 
+import { StubEventEmitter } from '../../../../../libs/event-emitter/test/fixture/StubEventEmitter';
 import { JoinShareDealCommand } from '../../../src/module/share-deal/application/port/in/dto/JoinShareDealCommand';
 import { OpenShareDealCommand } from '../../../src/module/share-deal/application/port/in/dto/OpenShareDealCommand';
+import { StartShareDealCommand } from '../../../src/module/share-deal/application/port/in/dto/StartShareDealCommand';
 import { NotJoinableShareDealException } from '../../../src/module/share-deal/application/port/in/exception/NotJoinableShareDealException';
 import { ShareDealQueryRepositoryPort } from '../../../src/module/share-deal/application/port/out/ShareDealQueryRepositoryPort';
 import { ShareDealRepositoryPort } from '../../../src/module/share-deal/application/port/out/ShareDealRepositoryPort';
 import { ShareDealCommandService } from '../../../src/module/share-deal/application/service/ShareDealCommandService';
+import { ShareDealStartedEvent } from '../../../src/module/share-deal/domain/event/ShareDealStartedEvent';
 import { FoodCategory } from '../../../src/module/share-deal/domain/vo/FoodCategory';
+import { ParticipantInfo } from '../../../src/module/share-deal/domain/vo/ParticipantInfo';
 import { ShareDealStatus } from '../../../src/module/share-deal/domain/vo/ShareDealStatus';
 import { assertResolvesLeft, assertResolvesRight } from '../../fixture';
 import { ShareDealFactory } from '../../fixture/ShareDealFactory';
@@ -15,14 +20,17 @@ import { ShareDealFactory } from '../../fixture/ShareDealFactory';
 describe('ShareDealCommandService', () => {
   const shareDealRepositoryPort = mock<ShareDealRepositoryPort>();
   const shareDealQueryRepositoryPort = mock<ShareDealQueryRepositoryPort>();
+  const eventEmitter = new StubEventEmitter();
   const shareDealCommandService = new ShareDealCommandService(
     shareDealRepositoryPort,
     shareDealQueryRepositoryPort,
+    eventEmitter,
   );
 
   beforeEach(() => {
     mockReset(shareDealRepositoryPort);
     mockReset(shareDealQueryRepositoryPort);
+    eventEmitter.clear();
   });
 
   describe('open', () => {
@@ -87,6 +95,50 @@ describe('ShareDealCommandService', () => {
       // then
       await assertResolvesRight(result, () => {
         expect(shareDeal.participantInfo.hasId(command.userId)).toBe(true);
+      });
+    });
+  });
+
+  describe('start', () => {
+    it('방장은 공유딜을 시작할 수 있다', async () => {
+      // given
+      const command = new StartShareDealCommand('userId', 'shareDealId');
+      const shareDeal = ShareDealFactory.create({
+        status: ShareDealStatus.OPEN,
+        ownerId: command.userId,
+        participantInfo: ParticipantInfo.of(['1'], 2),
+      });
+
+      shareDealQueryRepositoryPort.findById.mockReturnValue(right(shareDeal));
+      shareDealRepositoryPort.save.mockReturnValue(right(shareDeal));
+
+      // when
+      const result = shareDealCommandService.start(command);
+
+      // then
+      await assertResolvesRight(result, () => {
+        expect(eventEmitter.get(ShareDealStartedEvent.EVENT_NAME)).toBe(
+          command.shareDealId,
+        );
+        expect(shareDeal.status).toBe(ShareDealStatus.START);
+      });
+    });
+
+    it('OPEN 상태가 아닌 경우 시작할 수 없다', async () => {
+      // given
+      const command = new StartShareDealCommand('userId', 'shareDealId');
+      const shareDeal = ShareDealFactory.create({
+        status: ShareDealStatus.END,
+      });
+
+      shareDealQueryRepositoryPort.findById.mockReturnValue(right(shareDeal));
+
+      // when
+      const result = shareDealCommandService.start(command);
+
+      // then
+      await assertResolvesLeft(result, (error) => {
+        expect(error).toBeInstanceOf(IllegalStateException);
       });
     });
   });

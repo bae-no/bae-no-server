1add69df0c94f462846557e6e55a90ab349d7952
Yohan <cd80@kakao.com>
2022-11-10 20:55:06 +0900

    ✨ 공유딜 시작 메시지 이벤트 핸들러 추가
    Co-authored-by: Jake Son <sonjeabin@gmail.com>
    Co-authored-by: minwoo <mwoodev526@gmail.com>

--- apps/app/src/module/chat/adapter/in/listener/ChatEventListener.ts
+++ apps/app/src/module/chat/adapter/in/listener/ChatEventListener.ts
@@ -1,7 +1,12 @@
+import { TE } from '@app/custom/fp-ts';
 import { PubSubPort } from '@app/domain/pub-sub/PubSubPort';
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
+import { pipe } from 'fp-ts/function';
 
+import { ShareDealQueryRepositoryPort } from '../../../../share-deal/application/port/out/ShareDealQueryRepositoryPort';
+import { ShareDealStartedEvent } from '../../../../share-deal/domain/event/ShareDealStartedEvent';
+import { ChatRepositoryPort } from '../../../application/port/out/ChatRepositoryPort';
 import { Chat } from '../../../domain/Chat';
 import { ChatWrittenEvent } from '../../../domain/event/ChatWrittenEvent';
 import { ChatWrittenResponse } from '../gql/response/ChatWrittenResponse';
@@ -9,13 +14,37 @@ import { ChatWrittenTrigger } from './ChatWritttenTrigger';
 
 @Injectable()
 export class ChatEventListener {
-  constructor(private readonly pubSubPort: PubSubPort) {}
+  constructor(
+    private readonly pubSubPort: PubSubPort,
+    private readonly shareDealQueryRepositoryPort: ShareDealQueryRepositoryPort,
+    private readonly chatRepositoryPort: ChatRepositoryPort,
+  ) {}
 
-  @OnEvent(ChatWrittenEvent.EVENT_NAME)
+  @OnEvent(ChatWrittenEvent.EVENT_NAME, { async: true })
   async handleChatWrittenEvent(payload: Chat) {
     this.pubSubPort.publish(
       ChatWrittenTrigger(payload.shareDealId),
       ChatWrittenResponse.of(payload.message),
     );
+  }
+
+  @OnEvent(ShareDealStartedEvent.EVENT_NAME, { async: true })
+  async handleShareDealStartedEvent(shareDealId: string) {
+    await pipe(
+      this.shareDealQueryRepositoryPort.findById(shareDealId),
+      TE.map((shareDeal) =>
+        Chat.createByStartShareDeal(
+          shareDeal.id,
+          shareDeal.participantInfo.ids,
+        ),
+      ),
+      TE.chain((chats) => this.chatRepositoryPort.create(chats)),
+      TE.map((chat) =>
+        this.pubSubPort.publish(
+          ChatWrittenTrigger(shareDealId),
+          ChatWrittenResponse.of(chat[0].message),
+        ),
+      ),
+    )();
   }
 }

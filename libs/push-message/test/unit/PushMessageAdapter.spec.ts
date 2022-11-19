5a52d8d89bfbc427c4116a4fe8ed43c04fab91ba
Jake Son <sonjeabin@gmail.com>
2022-11-19 11:42:55 +0900

    ✅ PushMessageAdapter 테스트 추가

---
+++ libs/push-message/test/unit/PushMessageAdapter.spec.ts
@@ -0,0 +1,43 @@
+import { PushMessageAdapter } from '@app/push-message/PushMessageAdapter';
+import { Messaging } from 'firebase-admin/lib/messaging';
+import { mock } from 'jest-mock-extended';
+
+import {
+  assertResolvesLeft,
+  assertResolvesRight,
+} from '../../../../apps/app/test/fixture/utils';
+
+describe('PushMessageAdapter', () => {
+  const messaging = mock<Messaging>();
+  const pushMessageAdapter = new PushMessageAdapter(messaging);
+
+  it('푸시 메시지 전송에 성공한다', async () => {
+    // given
+    const pushToken = 'pushToken';
+    const content = 'content';
+
+    messaging.send.mockResolvedValue('messageId');
+
+    // when
+    const result = pushMessageAdapter.send(pushToken, content);
+
+    // then
+    await assertResolvesRight(result);
+  });
+
+  it('푸시 메시지 전송에 실패하면 에러를 반환한다', async () => {
+    // given
+    const pushToken = 'pushToken';
+    const content = 'content';
+
+    messaging.send.mockRejectedValue('error');
+
+    // when
+    const result = pushMessageAdapter.send(pushToken, content);
+
+    // then
+    await assertResolvesLeft(result, (err) => {
+      expect(err.message).toBe('error');
+    });
+  });
+});

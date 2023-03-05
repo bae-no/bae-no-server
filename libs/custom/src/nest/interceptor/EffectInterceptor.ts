76b6f33bc4166c16d8fee784d7f87b8e6f5ef5f5
Jake Son <sonjeabin@gmail.com>
2023-03-05 10:55:01 +0900

    ♻️ 커스텀 span 이 요청 span 하위에 위치하도록 수정

--- libs/custom/src/nest/interceptor/EffectInterceptor.ts
+++ libs/custom/src/nest/interceptor/EffectInterceptor.ts
@@ -1,11 +1,14 @@
 import { T, pipe } from '@app/custom/effect';
 import { liveTracer } from '@app/monitoring/init';
+import { Span, SpanImpl } from '@effect-ts/otel';
 import type {
   CallHandler,
   ExecutionContext,
   NestInterceptor,
 } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
+import type { Span as ApiSpan } from '@opentelemetry/api';
+import { trace } from '@opentelemetry/api';
 import type { Observable } from 'rxjs';
 import { mergeMap } from 'rxjs';
 
@@ -15,7 +18,14 @@ export class EffectInterceptor implements NestInterceptor {
     return next.handle().pipe(
       mergeMap(async (value) => {
         if (value instanceof T.Base) {
-          return pipe(value, liveTracer, T.runPromise);
+          return pipe(
+            value,
+            T.provideService(Span)(
+              new SpanImpl(trace.getActiveSpan() as ApiSpan),
+            ),
+            liveTracer,
+            T.runPromise,
+          );
         }
 
         return value;

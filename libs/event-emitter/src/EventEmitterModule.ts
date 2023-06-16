96bbfc05ea5c5ff769da9112d1e12b9cfeb18f14
Yohan <cd80@kakao.com>
2023-06-16 20:33:11 +0900

    ✨ BullMQ root module 등록
    Co-authored-by: Jake Son <sonjeabin@gmail.com>

--- libs/event-emitter/src/EventEmitterModule.ts
+++ libs/event-emitter/src/EventEmitterModule.ts
@@ -1,11 +1,29 @@
 import { EventEmitterPort } from '@app/domain/event-emitter/EventEmitterPort';
 import { EventEmitterAdapter } from '@app/event-emitter/EventEmitterAdapter';
+import { BullModule } from '@nestjs/bullmq';
 import { Global, Module } from '@nestjs/common';
-import { EventEmitterModule as EMM } from '@nestjs/event-emitter';
+import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({
-  imports: [EMM.forRoot()],
+  imports: [
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const connection: string = configService.getOrThrow(
+          'REDIS_CONNECTION_NAME',
+        );
+        const parts = connection.split(':');
+
+        return {
+          connection: {
+            host: parts.slice(0, -1).join(''),
+            port: Number(parts.at(-1)),
+          },
+        };
+      },
+    }),
+  ],
   providers: [{ provide: EventEmitterPort, useClass: EventEmitterAdapter }],
   exports: [EventEmitterPort],
 })

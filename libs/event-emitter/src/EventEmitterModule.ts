11c4706bdbd29e75c4d26b592d46d4ee06820dd2
Jake Son <sonjeabin@gmail.com>
2023-06-17 20:30:06 +0900

    ✨ 도메인 이벤트 디스패처 구현
    Co-authored-by: imdudu1 <cd80@kakao.com>

--- libs/event-emitter/src/EventEmitterModule.ts
+++ libs/event-emitter/src/EventEmitterModule.ts
@@ -1,12 +1,15 @@
 import { EventEmitterPort } from '@app/domain/event-emitter/EventEmitterPort';
+import { EventDispatcher } from '@app/event-emitter/EventDispatcher';
 import { EventEmitterAdapter } from '@app/event-emitter/EventEmitterAdapter';
 import { BullModule } from '@nestjs/bullmq';
 import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DiscoveryModule } from '@nestjs/core';
 
 @Global()
 @Module({
   imports: [
+    DiscoveryModule,
     BullModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
@@ -27,7 +30,10 @@ import { ConfigService } from '@nestjs/config';
       name: 'event-emitter',
     }),
   ],
-  providers: [{ provide: EventEmitterPort, useClass: EventEmitterAdapter }],
+  providers: [
+    { provide: EventEmitterPort, useClass: EventEmitterAdapter },
+    EventDispatcher,
+  ],
   exports: [EventEmitterPort],
 })
 export class EventEmitterModule {}
